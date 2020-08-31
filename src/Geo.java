d244396715b969a7699156019d67660cd7f7699b
Davi-K-Silva <davi.KDS.01@gmail.com>
2020-08-31 17:08:54 -0300

    Método de distancia não estático adicionado

--- src/Geo.java
+++ src/Geo.java
@@ -15,6 +15,20 @@ public class Geo {
         return longitude;
     }
 
+    public double distancia(Geo destiny){
+        double R = 6371;
+        double lat1 = Math.toRadians(latitude);
+        double lat2 = Math.toRadians(destiny.getLatitude());
+        double lon1 = Math.toRadians(longitude);
+        double lon2 = Math.toRadians(destiny.getLongitude());
+
+        double a = Math.pow(Math.sin((lat2 - lat1)/2), 2) +
+                Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin((lon2 - lon1)/2), 2);
+        double c = 2 * Math.atan(Math.sqrt(a) / Math.sqrt(1-a));
+        double d = R * c ;
+        return d;
+    }
+
     public static double distancia(Geo origin, Geo destiny){
         double R = 6371;
         double lat1 = Math.toRadians(origin.getLatitude());
